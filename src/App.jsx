47a796c0b48f02f3e205a63e002c483e96931a86
vhangsan <veronicahangsan@gmail.com>
2023-09-07 21:15:24 -0700

    add key prop and validateDOMNesting()

--- src/App.jsx
+++ src/App.jsx
@@ -18,23 +18,28 @@ function App() {
   console.log([loading, plants])
   return (
     <table border={1}>
-      <tr>
-        <th>Latin Name</th>
-        <th>Actual Name</th>
-        <th>Pollinators</th>
-        <th>Suitable Conditions</th>
-        <th>Water Needs</th>
-      </tr>
-      {plants.map(plant => (
+      <thead>
         <tr>
-          <td>{plant.latin_name}</td>
-          <td>{plant.common_name}</td>
-          <td>{plant.associated_wildlife}</td>
-          <td>{plant.suitable_site_conditions}</td>
-          <td>{plant.water_needs}</td>
+          <th>Latin Name</th>
+          <th>Actual Name</th>
+          <th>Pollinators</th>
+          <th>Suitable Conditions</th>
+          <th>Water Needs</th>
         </tr>
-      ))}
-    </table>)
+      </thead>
+      <tbody>
+        {plants.map((plant, index) => (
+          <tr key={index}>
+            <td>{plant.latin_name}</td>
+            <td>{plant.common_name}</td>
+            <td>{plant.associated_wildlife}</td>
+            <td>{plant.suitable_site_conditions}</td>
+            <td>{plant.water_needs}</td>
+          </tr>
+          ))}
+      </tbody>
+    </table>
+    );
 }
 export default App;
 
